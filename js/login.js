d6f1951e623066dda6718119582078f43d1f2a60
FahimaAkther <saimakhondokar58@gmail.com>
2025-02-08 01:02:59 +0600

    login-1st

---
+++ js/login.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-login').addEventListener('click', function(event){
+   
+    event.preventDefault();
+// get the phone-number
+const phoneNumber =getElementById('phone-number').value;
+console.log(phoneNumber)
+
+})
